98fb4c3db5caae4ba3927a6d9154995c16701568
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2024-02-21 11:21:29 +0600

    ✔️ Complete Drum Kit

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -13,7 +13,18 @@ function playSound(e) {
 }
 
 const keys = Array.from(document.querySelectorAll('.key'));
-
 keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
-
 window.addEventListener('keydown', playSound);
+
+keys.forEach((key) =>
+  key.addEventListener('click', function (e) {
+    let value = key.getAttribute('data-key');
+    const audio = document.querySelector(`audio[data-key="${value}"]`);
+    const key_data = document.querySelector(`div[data-key="${value}"]`);
+    if (audio) {
+      key_data.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play();
+    }
+  })
+);
